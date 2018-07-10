097780751cafe31a17e142f511a181c114a7ce6c
mnmly <i.am@mnmly.com>
2018-07-09 22:30:05 -0700

    Added

--- demo.js
+++ demo.js
@@ -1,4 +1,12 @@
 import App from './index'
+import isInApp from './lib/DetectInApp'
+
 let app = new App()
 app.appendTo( document.body )
 app.engine.start()
+
+if ( isInApp ) {
+    setTimeout( () => {
+        app.resize()
+    }, 1000.0 )
+}
